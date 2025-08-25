6fae9341e1ccaa65882e95f0d2e579812bc46377
eyessha <abab484@aucklanduni.ac.nz>
2025-08-25 18:12:07 +1200

    grip_sensor functions complete

--- src/grip_sensor.cpp
+++ src/grip_sensor.cpp
@@ -9,16 +9,46 @@ HX711 gripScale;
 
 
 void initGripSensor() {
-    // Connect HX711
-    // Set after calibration 
-    // Set zero refrence 
+   gripScale.begin(GRIP_DOUT, GRIP_SCK);    // Connect HX711
+   gripScale.set_scale();                   // Set after calibration 
+   gripScale.tare();                        // Set zero refrence 
 }
 
 float readGripForce(){
-    // Read one averaged value 
-    // If not ready, return 0
+   if(gripScale.is_ready())   {             
+return gripScale.get_units(1);              // Read one averaged value 
+   } else {
+    return 0;                                // If not ready, return 0
+   }              
+   
 }
 
 void runGripTest() {
-    
+    unsigned long startTime = millis();
+    unsigned long duration = 10000;            // 10 seconds
+
+    float maxForce = 0;
+    float currentForce = 0;
+
+    while (millis() - startTime < duration) {
+        currentForce = readGripForce();
+        if (currentForce > maxForce) maxForce = currentForce;
+
+        float fatigue = calculateFatigue(currentForce, maxForce);
+
+        Serial.print("Current: ");
+        Serial.print(currentForce);
+        Serial.print(" | Max: ");
+        Serial.print(maxForce);
+        Serial.print(" | Fatigue: ");
+        Serial.print(fatigue);
+        Serial.println(" %");
+
+        delay(100);                         // Sample ~10x per second
+
+    }
+
+    Serial.println(" Grip test complete.");
+    Serial.print(" Final Max Force: ");
+    Serial.println(maxForce);
 }
